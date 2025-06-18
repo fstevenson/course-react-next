136fad1a5600082651e0f22642a376f734952657
Fernando Stevenson <fernandosv88@gmail.com>
2025-06-17 20:47:27 -0600

    Added separator between title and list of tickets

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -10,6 +10,7 @@ import {
     CardDescription,
     CardContent,
 } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 
 
 import { ticketPath } from "@/paths";
@@ -47,6 +48,7 @@ const TicketsPage = () => {
                 <h2 className="text-3xl font-bold tracking-tight">Tickets Page</h2>
                 <p className="text-sm text-muted-foreground">All your tickets at one place</p>
             </div>
+            <Separator />
             <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
                 {initialTickets.map((ticket) => (
                     <Card key={ticket.id} className="w-full max-w-[420px]">
