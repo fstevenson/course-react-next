ede00f399090a7c091623cef410e70f35fa4fe14
Fernando Stevenson <fernandosv88@gmail.com>
2025-06-19 09:37:34 -0600

    Added theme provider and switcher and hook to fetch tickets

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,33 +2,39 @@ import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button"
 import { homePath, ticketsPath } from "@/paths";
 import { LucideKanban } from "lucide-react";
+import { ThemeSwitcher } from "./theme/theme-switcher";
 
 
 const Header = () => {
     return (
         <nav
             className="
-            supports-backdrop-blur:bg-background/60
-            fixed left-0 right-0 top-0 z-20
-            border-b bg-background/95 backdrop-blur
-            w-full flex py-2.5 px-5 justify-between
-          "
+        supports-backdrop-blur:bg-background/60
+        fixed left-0 right-0 top-0 z-20
+        border-b bg-background/95 backdrop-blur
+        w-full flex py-2.5 px-5 justify-between
+      "
         >
-            <div>
-                <Link href={homePath()} className={buttonVariants({ variant: "ghost" })}>
+            <div className="flex gap-x-2">
+                <Link
+                    href={homePath()}
+                    className={buttonVariants({ variant: "ghost" })}
+                >
                     <LucideKanban />
-                    <h1 className="ml-2 text-lg font-semibold">Ticket Bounty</h1>
+                    <h1 className="text-lg font-semibold">TicketBounty</h1>
                 </Link>
             </div>
-            <div>
-                { /*<Button asChild variant="outline">*/}
-                <Link href={ticketsPath()} className={buttonVariants({ variant: "default" })}>
+            <div className="flex gap-x-2">
+                <ThemeSwitcher />
+                <Link
+                    href={ticketsPath()}
+                    className={buttonVariants({ variant: "default" })}
+                >
                     Tickets
                 </Link>
-                {/*</Button>*/}
             </div>
         </nav>
-    )
-}
+    );
+};
 
 export { Header }
---
+++ src/features/ticket/queries/get-tickets.ts
@@ -0,0 +1,12 @@
+import { initialTickets } from "@/data"
+import { Ticket } from "../types";
+
+export const getTickets = async (): Promise<Ticket[]> => {
+
+    // Simulate a delay to mimic an API call
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    return new Promise((resolve) => {
+        resolve(initialTickets);
+    });
+};
