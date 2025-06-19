8e472fc78d6f26b205d254d115763a65241397be
Fernando Stevenson <fernandosv88@gmail.com>
2025-06-19 11:29:36 -0600

    Added react-error-boundary pkg to have more granularity on the error message

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -3,14 +3,18 @@ import { Heading } from "@/components/heading";
 import { Suspense } from "react";
 import { TicketList } from "@/features/ticket/components/ticket-list";
 import { Spinner } from "@/features/ticket/components/spinner";
+import { ErrorBoundary } from "react-error-boundary";
+import { Placeholder } from "@/components/placeholder";
 
 const TicketsPage = () => {
     return (
         <div className="flex-1 flex flex-col gap-y-8">
             <Heading title="Tickets Page" description="All your tickets at one place" />
-            <Suspense fallback={<Spinner />}>
-                <TicketList />
-            </Suspense>
+            <ErrorBoundary fallback={<Placeholder label="Something went wrong while loading tickets" />}>
+                <Suspense fallback={<Spinner />}>
+                    <TicketList />
+                </Suspense>
+            </ErrorBoundary>
         </div >
     );
 }
--- src/features/ticket/queries/get-tickets.ts
+++ src/features/ticket/queries/get-tickets.ts
@@ -2,7 +2,6 @@ import { initialTickets } from "@/data"
 import { Ticket } from "../types";
 
 export const getTickets = async (): Promise<Ticket[]> => {
-
     // Simulate a delay to mimic an API call
     await new Promise((resolve) => setTimeout(resolve, 2000));
 
