2a3711d8b81f573e67330c263c80820881e48a61
Fernando Stevenson <fernandosv88@gmail.com>
2025-06-24 21:41:38 -0600

    Added form to create a new ticket

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -3,8 +3,10 @@ import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import { Heading } from "@/components/heading";
 import { Placeholder } from "@/components/placeholder";
-import { Spinner } from "@/features/ticket/components/spinner";
+import { Spinner } from "@/components/spinner";
 import { TicketList } from "@/features/ticket/components/ticket-list";
+import { TicketCreateForm } from "@/features/ticket/components/ticket-create-form";
+import { CardCompact } from "@/components/card-compact";
 
 // export const dynamic = "force-dynamic"; // Force dynamic rendering to avoid stale data issues
 // export const revalidate = 30; // revalidate to fetch fresh data
@@ -13,6 +15,10 @@ const TicketsPage = () => {
     return (
         <div className="flex-1 flex flex-col gap-y-8">
             <Heading title="Tickets Page" description="All your tickets at one place" />
+            <CardCompact
+                title="Create Ticket"
+                description="Create a new ticket to get help"
+                className="w-full max-w-[420px] self-center" content={<TicketCreateForm />} />
             <ErrorBoundary fallback={<Placeholder label="Something went wrong while loading tickets" />}>
                 <Suspense fallback={<Spinner />}>
                     <TicketList />
