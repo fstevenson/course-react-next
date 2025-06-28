5e4e20475c986ff9240c7adeada0a405d42dce12
Fernando Stevenson <fernandosv88@gmail.com>
2025-06-27 20:58:54 -0600

    Merged update and create ticket form and action into a single one called upsert

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -7,6 +7,7 @@ import { Spinner } from "@/components/spinner";
 import { TicketList } from "@/features/ticket/components/ticket-list";
 import { TicketCreateForm } from "@/features/ticket/components/ticket-create-form";
 import { CardCompact } from "@/components/card-compact";
+import { TicketUpsertForm } from "@/features/ticket/components/ticket-upsert-form";
 
 // export const dynamic = "force-dynamic"; // Force dynamic rendering to avoid stale data issues
 // export const revalidate = 30; // revalidate to fetch fresh data
@@ -18,7 +19,8 @@ const TicketsPage = () => {
             <CardCompact
                 title="Create Ticket"
                 description="Create a new ticket to get help"
-                className="w-full max-w-[420px] self-center" content={<TicketCreateForm />} />
+                className="w-full max-w-[420px] self-center"
+                content={<TicketUpsertForm />} />
             <ErrorBoundary fallback={<Placeholder label="Something went wrong while loading tickets" />}>
                 <Suspense fallback={<Spinner />}>
                     <TicketList />
---
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -0,0 +1,27 @@
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { Ticket } from "@/generated/prisma/client";
+import { upsertTicket } from "@/features/ticket/actions/upsert-ticket";
+
+type TicketUpsertFormProps = {
+    ticket?: Ticket
+}
+
+const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
+    return (
+        <form action={upsertTicket.bind(null, ticket?.id)} className="flex flex-col gap-y-2">
+            <Label htmlFor="title">Title</Label>
+            <Input type="text" id="title" name="title" defaultValue={ticket?.title} />
+
+            <Label htmlFor="content">Content</Label>
+            <Textarea id="content" name="content" defaultValue={ticket?.content} />
+
+            <Button type="submit">{ticket ? "Edit" : "Create"}</Button>
+        </form>
+    );
+}
+
+export { TicketUpsertForm }
